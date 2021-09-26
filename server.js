cbd17a549f08a85fa2793764c494a4d3bdef3dc3
Reno P P <renoputraprawira@gmail.com>
2021-09-26 07:13:10 +0700

    fix

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const router = jsonServer.router("db.json");
 const middlewares = require("json-server-auth");
 const port = process.env.PORT || 3000;
 
+// /!\ Bind the router db to the app
+server.db = router.db
+
 server.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "*");
