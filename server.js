177ad08b7f2a40f6940667579a9dc68f6c3372bb
Reno P P <renoputraprawira@gmail.com>
2021-09-26 07:03:59 +0700

    resolve error cors

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
-const jsonServer = require('json-server');
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = require('json-server-auth');
+const router = jsonServer.router("db.json");
+const middlewares = require("json-server-auth");
 const port = process.env.PORT || 3000;
+
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
 
 server.use(middlewares);
 server.use(router);
